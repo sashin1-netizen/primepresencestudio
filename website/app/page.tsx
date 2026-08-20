1963377a5d2e56fe16bc9410a8ca74757a7deee2
sashin1-netizen <sashinramdhanee1@gmail.com>
2026-08-20 23:20:00 +0200

    Replace fragmented homepage with unified Atelier experience

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,33 +1,13 @@
 import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import StoryPanel from "../components/StoryPanel";
-import Services from "../components/Services";
-import WebsiteShowcase from "../components/cinematic/WebsiteShowcase";
-import Portfolio from "../components/Portfolio";
-import Process from "../components/Process";
-import PremiumCTA from "../components/cinematic/PremiumCTA";
+import AtelierHome from "../components/cinematic/AtelierHome";
 import Footer from "../components/Footer";
 import { site } from "@/content/site";
-import { publishedMediaById } from "@/content/media";
 
 export default function Home() {
-  const heroMedia = publishedMediaById("home-hero");
-
   return (
     <>
       <Navbar siteName={site.name} />
-      <main id="main-content" className="overflow-hidden bg-[#050505] text-white">
-        <Hero
-          location={site.location}
-          media={heroMedia ? { src: heroMedia.src, alt: heroMedia.alt, decorative: heroMedia.decorative } : undefined}
-        />
-        <StoryPanel />
-        <Services />
-        <WebsiteShowcase />
-        <Portfolio />
-        <Process />
-        <PremiumCTA />
-      </main>
+      <AtelierHome location={site.location} />
       <Footer />
     </>
   );
